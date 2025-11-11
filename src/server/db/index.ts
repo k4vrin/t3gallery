1a9e143bef4a87b2f02082ea7fd4398e836a16a9
Mostafa Hosseini <kavrindev@gmail.com>
2025-11-11 21:34:10 +0330

    Feat: Update database connection logic to use environment variables
    This change improves the database connection setup by utilizing environment variables for configuration.
    
    Key changes include:
    • Updated the database connection logic in `index.ts`:
    • Replaced direct access to `process.env.DATABASE_URL` with `env.DATABASE_URL` for better environment management.
    • Ensured that the configuration is loaded from the `.env` file.

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,9 +2,10 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
+import { env } from "~/env";
 import * as schema from "./schema";
 
 config({ path: ".env" }); // or .env.local
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(env.DATABASE_URL);
 export const db = drizzle(sql, { schema });
